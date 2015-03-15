dfe5ef431ec11f324b674b159c971ed38baae28f
Ben Koziol <benkoziol@gmail.com>
2015-03-14 20:35:54 -0600

    mark - removed gdal_data requirement

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -2,9 +2,4 @@ import os
 import subprocess
 
 
-curr = os.getcwd()
-try:
-    os.chdir(os.environ['SRC_DIR'])
-    subprocess.check_call([os.environ['PYTHON'], 'setup.py', 'test'])
-finally:
-    os.chdir(curr)
+subprocess.check_call(['cd', os.environ['SRC_DIR'], '&&', os.environ['PYTHON'], 'setup.py', 'test'])
