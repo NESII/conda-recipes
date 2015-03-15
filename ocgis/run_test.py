7eb563e5ceecca23e794baad34daf6ae77bb47d8
Ben Koziol <benkoziol@gmail.com>
2015-03-14 20:57:40 -0600

    mark - removed gdal_data requirement

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -3,9 +3,9 @@ from nose.plugins.plugintest import run
 import ocgis
 from ocgis.test import test_simple
 import sys
+import os
 
 
-module_name = test_simple.__file__
-print(module_name)
+module_name = os.path.split(test_simple.__file__)[0]
 
 nose.run(argv=[sys.argv[0], module_name, '-v'])
