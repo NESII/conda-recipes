b48490addf73a19425410aee0eae5a29208db320
Ben Koziol <benkoziol@gmail.com>
2015-03-16 10:29:46 -0600

    mark - removed gdal_data requirement

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -6,9 +6,9 @@ import sys
 import os
 
 from cfunits import Units
-# module_name = os.path.split(test_simple.__file__)[0]
+module_name = os.path.split(test_simple.__file__)[0]
 from ocgis.test.test_misc import test_dependency_versions
 
-module_name = test_dependency_versions.__file__
+# module_name = test_dependency_versions.__file__
 
 nose.run(argv=[sys.argv[0], module_name, '-v'])
