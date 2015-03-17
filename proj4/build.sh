f1c8e74a6defdf285e96544d175686f947f6426a
Ben Koziol <benkoziol@gmail.com>
2015-03-17 15:43:39 -0600

    mark - removed gdal_data requirement

--- proj4/build.sh
+++ proj4/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+DFN = proj-datumgrid-1.5.zip
+wget wget http://download.osgeo.org/proj/${DFN}
+unzip ${DFN} -d ./nad/
+
 ./configure --prefix=$PREFIX --without-jni
 
 make -j ${CPUCOUNT}
