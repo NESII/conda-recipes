c68dc54f444602b4d215c27afdd9720e1f1f1afb
Ben Koziol <benkoziol@gmail.com>
2015-03-17 11:33:11 -0600

    mark - removed gdal_data requirement

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -11,10 +11,7 @@ from ocgis.test.test_misc import test_dependency_versions
 #tdk: make this work for simple and all tests
 # module_name = test_dependency_versions.__file__
 os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser('~/data/ocgis_test_data/shp')
-os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser('~/data/ocgis_test_data/nc')
-from ocgis import env
-print(env.DIR_TEST_DATA)
-sys.exit(1)
+os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser('~/data/ocgis_test_data')
 result = nose.run(argv=[sys.argv[0], module_name, '-v', '-a', '!slow,!remote,!esmpy7'])
 if not result:
     sys.exit(1)
