1b6a2d06cb1f969739ab6dc20f970fbdf3d73710
Ben Koziol <benkoziol@gmail.com>
2015-03-17 17:03:11 -0600

    mark - removed gdal_data requirement

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -6,15 +6,17 @@ import sys
 import os
 from ocgis.test.test_ocgis.test_interface.test_base import test_crs
 from ocgis.test.test_real_data import test_narccap
-
 from cfunits import Units
-module_name = test_crs.__file__+':TestCFRotatedPole'
-print(module_name)
 from ocgis.test.test_misc import test_dependency_versions
+
+
 #tdk: make this work for simple and all tests
+# module_name = test_crs.__file__+':TestCFRotatedPole'
 # module_name = test_dependency_versions.__file__
+module_name = os.path.split(test.__file__)[0]
+
 os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser('~/data/ocgis_test_data/shp')
 os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser('~/data/ocgis_test_data')
-result = nose.run(argv=[sys.argv[0], module_name, '-vx', '-a', '!slow,!remote,!esmpy7'])
+result = nose.run(argv=[sys.argv[0], module_name, '-v', '-a', '!slow,!remote,!esmpy7'])
 if not result:
     sys.exit(1)
