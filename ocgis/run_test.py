9709ec36f2d7599bbbc67e003c3331db313013de
Ben Koziol <benkoziol@gmail.com>
2015-03-20 10:55:55 -0600

    mark - removed gdal_data requirement

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -8,6 +8,7 @@ from ocgis.test.test_ocgis.test_interface.test_base import test_crs
 from ocgis.test.test_real_data import test_narccap
 from cfunits import Units
 from ocgis.test.test_misc import test_dependency_versions
+import multiprocessing
 
 
 #tdk: make this work for simple and all tests
@@ -18,6 +19,6 @@ module_name = os.path.split(test.__file__)[0]
 os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser('~/data/ocgis_test_data/shp')
 os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser('~/data/ocgis_test_data')
 result = nose.run(argv=[sys.argv[0], module_name, '-v', '-a', '!slow,!remote,!esmpy7',
-                  '--processes={0}'.format(os.environ['CPUCOUNT'])])
+                  '--processes={0}'.format(multiprocessing.cpu_count())])
 if not result:
     sys.exit(1)
