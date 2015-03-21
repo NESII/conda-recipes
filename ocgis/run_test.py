e69f270aa2759f37608f980c4b72dca8e9828d38
Ben Koziol <benkoziol@gmail.com>
2015-03-21 10:26:49 -0600

    mark - removed gdal_data requirement

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -1,23 +1,27 @@
-import nose
-from nose.plugins.plugintest import run
-import ocgis
-from ocgis import test
-import sys
-import os
-from ocgis.test.test_ocgis.test_interface.test_base import test_crs
-from ocgis.test.test_real_data import test_narccap
-from cfunits import Units
-from ocgis.test.test_misc import test_dependency_versions
-import multiprocessing
+# import nose
+# from nose.plugins.plugintest import run
+# import ocgis
+# from ocgis import test
+# import sys
+# import os
+# from ocgis.test.test_ocgis.test_interface.test_base import test_crs
+# from ocgis.test.test_real_data import test_narccap
+# from cfunits import Units
+# from ocgis.test.test_misc import test_dependency_versions
+# import multiprocessing
 
 
 #tdk: make this work for simple and all tests
 # module_name = test_crs.__file__+':TestCFRotatedPole'
 # module_name = test_dependency_versions.__file__
-module_name = os.path.split(test.__file__)[0]
+# module_name = os.path.split(test.__file__)[0]
+#
+# os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser('~/data/ocgis_test_data/shp')
+# os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser('~/data/ocgis_test_data')
+# result = nose.run(argv=[sys.argv[0], module_name, '-a', '!slow,!remote,!esmpy7'])
+# if not result:
+#     sys.exit(1)
 
-os.environ['OCGIS_DIR_SHPCABINET'] = os.path.expanduser('~/data/ocgis_test_data/shp')
-os.environ['OCGIS_DIR_TEST_DATA'] = os.path.expanduser('~/data/ocgis_test_data')
-result = nose.run(argv=[sys.argv[0], module_name, '-a', '!slow,!remote,!esmpy7'])
-if not result:
-    sys.exit(1)
+from ocgis.test import run
+
+run()
