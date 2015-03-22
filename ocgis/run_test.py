f866598f3700ee4d9d64fec8eae39d9e3b577cac
Ben Koziol <benkoziol@gmail.com>
2015-03-22 10:44:23 -0600

    mark - made simple tests the default target

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -1,4 +1,13 @@
 from ocgis.test import run
+import os
 
-#tdk: make this work for simple and all tests
-run()
+
+test_target = os.environ.get('CBUILD_OCGIS_TEST_TARGET')
+if test_target == 'simple' or test_target is None:
+    simple = True
+elif test_target == 'all':
+    simple = False
+else:
+    raise NotImplementedError(test_target)
+
+run(simple=simple)
