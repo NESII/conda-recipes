81aeb306afd55e2d42fb1aaa801138457d061157
Ben Koziol <benkoziol@gmail.com>
2015-03-23 08:48:00 -0600

    mark - made simple tests the default target

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -1,8 +1,6 @@
 from ocgis.test import run
 import os
-import subprocess
-print(subprocess.check_call(['which', 'python']))
-tdk
+
 
 test_target = os.environ.get('CBUILD_OCGIS_TEST_TARGET')
 if test_target == 'simple' or test_target is None:
